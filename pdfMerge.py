dbed113228c28c72c196086e9c54572ae8b493dd
oresome <113960903+oresome@users.noreply.github.com>
2023-08-21 09:48:24 +0800

    Update pdfMerge.py
    added video download

--- pdfMerge.py
+++ pdfMerge.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from PyPDF2 import PdfMerger, PdfReader
 from io import BytesIO
+import requests
 
 def merge_pdfs(pdfFiles):
     merger = PdfMerger()
@@ -13,6 +14,11 @@ def merge_pdfs(pdfFiles):
     _byteIo.seek(0)
     
     return _byteIo
+
+def download_file(file_url, file_name):
+    response = requests.get(file_url)
+    with open(file_name, 'wb') as file:
+        file.write(response.content)
 
 def main():
     st.title("PDF Document Merge APP")
@@ -29,6 +35,17 @@ def main():
 
         # Download button
         st.download_button("Download Merged PDF", data=merged_file, file_name="merged.pdf",mime='application/octet-stream')
+
+    st.markdown("---")
+    st.markdown("Demo video download")
+    st.video("Bisalloy_demo.mp4")
+
+    file_url = 'https://webify-1306024390.cos.ap-shanghai.myqcloud.com/Download/Bisalloy_UH-RFID_Wear_Sensor_Demo.mp4'
+    #file_name = 'Bisalloy_demo.mp4'
+    file_data = requests.get(file_url).content
+    st.download_button('Download This Demo', data=file_data, file_name="Bisalloy_demo.mp4", mime='application/octet-stream')
+
+
 
 
 if __name__ == "__main__":
